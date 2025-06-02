5e1a858a15e3d02f617c7a0db32bb24a539e3354
Ihor Sofiichenko <asoluter@gmail.com>
2025-06-02 03:08:49 -0700

    Add wget/cacert

--- windows/scoop_setup.ps1
+++ windows/scoop_setup.ps1
@@ -8,8 +8,8 @@ scoop update
 scoop bucket add extras
 scoop bucket add java
 
-scoop install git 7zip which ripgrep aria2 curl cmder cmake ninja difftastic gcloud
-scoop install mpv neovim temurin-lts-jdk kdiff3 keepassxc foobar2000 gitextensions gimp inkscape go hxd vlc ffmpeg-shared
+scoop install git 7zip which ripgrep aria2 cacert curl wget cmder cmake ninja difftastic gcloud
+scoop install mpv neovim temurin-lts-jdk kdiff3 keepassxc foobar2000 gitextensions gimp inkscape go hxd vlc ffmpeg-shared sysinternals protobuf
 
 scoop update *
 scoop cleanup *
